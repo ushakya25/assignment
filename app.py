ebc6d4b2cf3a09900211976af35af8f2940f78be
uttam chand shakya <ushakyajob25@gmai.com>
2021-12-30 01:03:10 +0530

    Task 1 partial completed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import json
 
 app = Flask(__name__,template_folder='templates')
@@ -8,27 +8,36 @@ app = Flask(__name__,template_folder='templates')
 URL = "http://localhost:3000/employee"
 
 
-PARAMS = {
-    
-      "empname":"Deepak",
-      "company":"HCL",
-      "Salary":5000,
-      "Designatio":"Jr. Developer"
-    }
-    
-if len(PARAMS)<2:
-    print("Invalid Input Code:400")
-else:
-    r = requests.post(url = URL, data = PARAMS)
-    print('Valid Input Code:', r.status_code)
-
-
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+@app.route('/jsonapi', methods=['POST'])
+def validate_data():
+    f_name = request.form.get('fname')
+    l_name = request.form.get('lname')
+    company_name = request.form.get('cmp')
+    city = request.form.get('loc')
 
+    
+
+    load_data = {
+        "fname": f_name,
+        "lname": l_name,
+        "company": company_name,
+        "location": city
+    }
+
+    if len(load_data)<2:
+       
+        return "Bad Request Error Code:400"
+    else:
+        r = requests.post(url = URL, data = load_data)
+        print('Valid Input Code:', r.status_code)
+        return jsonify({'data':load_data}) , r.status_code
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
